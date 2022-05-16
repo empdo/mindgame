d5578a555a1ff49791e009bda74b7201315efe25
empdo <emil.essung@live.se>
2022-05-16 13:56:40 +0200

    add ability to add users to lobby

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import Koa from "koa";
 import koaBody from "koa-body";
 import KoaRouter from "@koa/router";
 import koaWs from "koa-easy-ws";
+import koaCors from "@koa/cors";
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -15,20 +16,74 @@ let lobbies: { [id: string]: Lobby } = {};
 class Player {
   name: string;
   ws: WebSocket;
+  readyState: boolean;
 
   constructor(name: string, ws: WebSocket) {
     this.name = name;
     this.ws = ws;
+    this.readyState = false;
   }
 }
 
 class Lobby {
   id: string;
-  players = [];
+  players: Player[] = [];
 
   constructor(id: string) {
     this.id = id;
   }
+
+  addPlayer = (player: Player) => {
+    if (!player.ws) return;
+
+    this.players.push(player);
+
+    player.ws.onclose = () => this.socketClose(player);
+    player.ws.onmessage = (e) => this.handleMessage(e, player);
+
+    this.alertPlayersList();
+  };
+
+  handleMessage = (e: MessageEvent<any>, player: Player) => {
+    if (!e.data) return;
+
+    const { type, data } = JSON.parse(e.data);
+
+    if (type === 1) {
+      player.readyState = !player.readyState;
+    }
+
+    this.alertPlayersList();
+  };
+
+  socketClose = (player: Player) => {
+    this.players = this.players.filter((p) => p.ws !== player.ws);
+    this.alertPlayersList();
+  };
+
+  gameloop = () => {
+    //skicka att de börjar till varej spelare
+    //de bygger sin scene
+    //de skickar okej tillbaka
+  };
+
+  alertPlayersList = () => {
+    //change to only update the thing that has changed
+    this.players.forEach((player) => {
+      player.ws.send(
+        JSON.stringify({
+          type: "1",
+          data: this.players.map((player) => {
+            return {
+              name: player.name,
+              readyState: player.readyState,
+            };
+          }),
+        })
+      );
+    });
+    console.log(lobbies);
+  };
 }
 
 router.get("/", (ctx) => {
@@ -36,29 +91,27 @@ router.get("/", (ctx) => {
 });
 
 router.get("/lobbies", (ctx) => {
-  ctx.body = lobbies;
+  ctx.body = Object.keys(lobbies);
 });
 
-router.post("/lobby/:id", (ctx) => {
-  //create lobby
-  const id = ctx.params.id;
-  if (!lobbies[id]) {
-    lobbies[id] = new Lobby(id);
-    console.log(`Added lobby with id: ${id}`);
-  } else {
-    ctx.body = "lobby already exists";
-  }
-});
-
-router.get("/lobby/:id/ws", async (ctx) => {
+router.get("/lobby/:id/", async (ctx) => {
   //connect to lobby
-  if (!ctx.ws) return;
+  const id = ctx.params.id;
+  if (!ctx.ws || !id) return;
 
   const ws: WebSocket = await ctx.ws();
+
+  if (!lobbies[id]) {
+    console.log(`Creating lobby with id: ${id}`);
+    lobbies[id] = new Lobby(id);
+  }
+
+  lobbies[id].addPlayer(new Player(ctx.request.hostname, ws));
 });
 
 app.use(koaBody());
 app.use(koaWs());
+app.use(koaCors());
 app.use(router.routes());
 app.listen(5000);
 
